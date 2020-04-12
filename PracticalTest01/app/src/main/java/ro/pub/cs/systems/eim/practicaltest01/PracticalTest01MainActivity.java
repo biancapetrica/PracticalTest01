9fb7fef1f383455b0bdd78ca63686ccd0db40644
biancapetrica <biancapetrica.g@gmail.com>
2020-04-12 18:02:57 +0300

    Add B1

--- PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
+++ PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
@@ -2,12 +2,49 @@ package ro.pub.cs.systems.eim.practicaltest01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01MainActivity extends AppCompatActivity {
+
+    private EditText leftEditText;
+    private EditText rightEditText;
+    private Button pressMeButton, pressMeTooButton;
+    private Button navigateToSecondaryActivityButton;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            int leftNumberOfClicks = Integer.valueOf(leftEditText.getText().toString());
+            int rightNumberOfClicks = Integer.valueOf(rightEditText.getText().toString());
+
+            switch(view.getId()) {
+                case R.id.press_me_button:
+                    leftNumberOfClicks++;
+                    leftEditText.setText(String.valueOf(leftNumberOfClicks));
+                    break;
+                case R.id.press_me_too_button:
+                    rightNumberOfClicks++;
+                    rightEditText.setText(String.valueOf(rightNumberOfClicks));
+                    break;
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_main);
+
+        leftEditText = (EditText)findViewById(R.id.left_edit_text);
+        rightEditText = (EditText)findViewById(R.id.right_edit_text);
+        pressMeButton = (Button)findViewById(R.id.press_me_button);
+        pressMeButton.setOnClickListener(buttonClickListener);
+        pressMeTooButton = (Button)findViewById(R.id.press_me_too_button);
+        pressMeTooButton.setOnClickListener(buttonClickListener);
     }
 }
